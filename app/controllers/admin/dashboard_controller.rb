88afad9e63faf36c6fef83e297c65f4f5f5fbd36
tynevosa <michael.edy623@gmail.com>
2023-01-26 09:10:56 -0500

    finished product and categories count cards for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASSWORD']
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
 end
